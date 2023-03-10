db12d53a5a06ee5d8d7972a93b36d5315b293e04
SoumyasriSadhula <soumyasrisadhula@gmail.com>
2023-03-10 11:20:03 +0530

    First Commit

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def f1(request):
 	return HttpResponse("<h1>Hello from DemoApp1 Github</h1><hr />");
 def f2(request):
-	return HttpResponse("<h1>Hello from DemoApp1 f2()</h1><hr />");
+	return HttpResponse("<h1>Hello from DemoApp1 Pycharm</h1><hr />");
